6dde85358ee72183599fd4d20522168a3ae61b97
Vincent Bernat <bernat@luffy.cx>
2011-11-25 11:39:32 +0100

    Add a new dataset. Compare with first value instead of client/server.

--- server-vs-client.py
+++ server-vs-client.py
@@ -3,8 +3,12 @@
 
 # Plot data from server-vs-client.
 
+import sys
+
 handshakes = 1000
 data = [
+    # Set 0
+    [
     ("RSA 1024",     0.991,   1.643),
     ("RSA 2048",     1.129,   6.576),
     ("RSA 4096",     1.524,  37.723),
@@ -16,7 +20,23 @@ data = [
     ("ECDH 2048",    4.477,   8.516),
     ("RSA 1024, e'", 1.062,   1.816),
     ("RSA 2048, e'", 1.311,   8.779),
+    ],
+    # Set 1
+    [
+    ("RSA 2048",        0.8792,  3.8064),
+    ("DHE 2048",       18.6667, 11.6786),
+    ("ECDHE 2048",      2.7034,  4.8524),
+    ("ECDHE-64 2048",  1.5394,  4.3936),
+    ("ECDHE 1024",      2.5844,  2.1432),
+    ("ECDHE-64 1024",  1.4560,  1.6936),
+#    ("ECDHE 4096",      2.2482, 18.5236),
+#    ("ECDHE-64 4096",  1.392,  18.5796),
+    ],
     ]
+data = data[int(sys.argv[1])]
+clientvsserver = [True, False]
+clientvsserver = clientvsserver[int(sys.argv[1])]
+
 
 from matplotlib.pylab import *
 from matplotlib.patches import Ellipse
@@ -58,18 +78,23 @@ for rect in client + server:
 
 # Write ratio
 for i in range(len(data)):
+    if not clientvsserver and i == 0:
+        continue
     width = server[i].get_width()
     if width > 3:
         xloc = width + 4
     else:
-        xloc = width + 7
+        xloc = width + 0.4*len(data) + 2.6
     yloc = server[i].get_y() + server[i].get_height()/2.0
-    ratio = data[i][2]/data[i][1]
+    if clientvsserver:
+        ratio = data[i][2]/data[i][1]
+    else:
+        ratio = data[i][2]/data[0][2]
     if ratio <= 2:
         ratio = "%+d %%" % (ratio*100-100)
     else:
         ratio = u"× %.1f" % ratio
-    el = Ellipse((xloc, yloc), 6, 0.57, edgecolor="white",
+    el = Ellipse((xloc, yloc), len(data)*0.4 + 1.6, len(data)*0.04 + 0.16, edgecolor="white",
                  facecolor="#8A9B0F", alpha=0.9)
     ax.add_artist(el)
     text(xloc, yloc, ratio,
